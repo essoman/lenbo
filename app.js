541211587a2f477e25ae2c9957d4f7fc11a5d5fc
essoMan <essoD@essod1.local>
2017-06-27 22:16:31 +0200

    modify credential to credentials

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var handlebars = require('express-handlebars') .create({ defaultLayout:'main' })
     app.engine('handlebars', handlebars.engine);
     app.set('view engine', 'handlebars');
 
+    app.use(require('body-parser').urlencoded({extended: true}));
+
 app.disable('x-powered-by');
 app.use(express.static(__dirname + '/public'));
 
@@ -13,6 +15,16 @@ app.get('/', function(req, res){
 });
 app.get('/contact', function(req, res){
     res.render('contact', {csrf: 'CSRF token here'});
+});
+app.get('/thankyou', function(req, res){
+    res.render('thankyou');
+});
+app.post('/process', function(req, res){
+    console.log('Form:  '  + req.query.form);
+    console.log('CSRF token : ' + req.body._csrf);
+    console.log('Email : '  + req.body.email);
+    console.log('Question: ' + req.body.ques);
+    res.redirect(303, '/thankyou');
 });
 
 app.listen(3000)
