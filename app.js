7cabaf496835586f6b3a9bcfa24ac9f0348c9543
essoMan <essoD@essod1.local>
2017-06-28 01:32:25 +0200

    Add a page counter

--- app.js
+++ app.js
@@ -2,26 +2,49 @@ var express     = require('express');
 var app         = express();
 
 var session     = require('express-session');
-var parseurl    = require(parseurl);
+var parseurl    = require('parseurl');
 var handlebars  = require('express-handlebars') .create({ defaultLayout:'main' });
     app.engine('handlebars', handlebars.engine);
     app.set('view engine', 'handlebars');
 
     app.use(require('body-parser').urlencoded({extended: true}));
 
-app.disable('x-powered-by');
-app.use(express.static(__dirname + '/public'));
+    app.disable('x-powered-by');
 
-app.get('/', function(req, res){
+    app.use(session({
+    resave: false,
+    saveUninitialized: true,
+    secret: credentials.cookieSecret,
+}));
+
+    app.use(function(req, res, next){
+        var views = req.session.views;
+
+        if(!views){
+            views = req.session.views = {};
+        }
+
+        var pathname = parseurl(req).pathname;
+        views[pathname] = (views[pathname]|| 0) + 1;
+
+        next()        
+    });
+
+    app.use(express.static(__dirname + '/public'));
+
+    app.get('/viewcount', function(req, res, next){
+    res.send('You viewed this page '+req.session.views['/viewcount']+ 'times');
+    });
+    app.get('/', function(req, res){
     res.render('home');
 });
-app.get('/contact', function(req, res){
+    app.get('/contact', function(req, res){
     res.render('contact', {csrf: 'CSRF token here'});
 });
-app.get('/thankyou', function(req, res){
+    app.get('/thankyou', function(req, res){
     res.render('thankyou');
 });
-app.post('/process', function(req, res){
+    app.post('/process', function(req, res){
     console.log('Form:  '  + req.query.form);
     console.log('CSRF token : ' + req.body._csrf);
     console.log('Email : '  + req.body.email);
@@ -29,4 +52,4 @@ app.post('/process', function(req, res){
     res.redirect(303, '/thankyou')
 });
 
-app.listen(3000)
+    app.listen(3000)
