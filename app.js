b387e03857c52e4493b5670bbd6d5a9c6b1198df
essoMan <essoD@essod1.local>
2017-06-27 21:45:11 +0200

    Temporary Redirection message added

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 var express     = require('express');
 var app         = express();
-var handlebars = require('express3-handlebars') .create({ defaultLayout:'main' });
+
+var handlebars = require('express-handlebars') .create({ defaultLayout:'main' });
     app.engine('handlebars', handlebars.engine);
     app.set('view engine', 'handlebars');
 
+app.disable('x-powered-by');
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res){
     res.render('home');
 });
-
+app.get('/contact', function(req, res){
+    res.render('contact', {csrf: 'CSRF token here'});
+});
 
 app.listen(3000)
